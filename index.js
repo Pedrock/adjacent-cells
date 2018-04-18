f8b6946731af0516ca04e4a11b9e54f8db36daa5
Pedro Câmara <camara-pedro@hotmail.com>
2018-04-18 10:26:09 +0100

    Adds print function

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ if (process.argv.length !== 3) {
     return;
 }
 
+function printGroups(result) {
+    for (const group of result) {
+        const groupsString = group
+        .map(coords => `[${coords.join(', ')}]`)
+        .join(", ")
+        console.log(`[ ${groupsString} ]`);
+    }
+}
+
 const fileName = process.argv[2];
 const inputFile = fs.readFileSync(fileName);
 const input = JSON.parse(inputFile);
 
-const result = new Solver(input).solve();
-console.log(result);
+const resultGroups = new Solver(input).solve();
+printGroups(resultGroups);
